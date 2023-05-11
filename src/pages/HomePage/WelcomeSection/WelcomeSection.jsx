7a8cf8b9728adb13e51f6b53eb64f8485b302e08
michudud <pzlc1236@gmail.com>
2023-05-11 22:12:13 +0200

    welcome section update

--- src/pages/HomePage/WelcomeSection/WelcomeSection.jsx
+++ src/pages/HomePage/WelcomeSection/WelcomeSection.jsx
@@ -6,7 +6,15 @@ const WelcomeSection = () => {
       <div className="bg_tint">
         <div className="content">
           <h1>Welcome!</h1>
-          <p></p>
+          <p>
+            We're glad you made it here and hope you're hungry for new flavors.
+            Our pizza is made with passion and attention to details, starting
+            from choosing the best ingredients to controlling the temperature in
+            the oven. We're sure you'll love our specialties and want to come
+            back for more, even if you don't like pizza (you sure you're human?
+            it doesn't matter, you're welcome anyway)
+          </p>
+          <h2>Pizza is Love, Pizza is Life.</h2>
         </div>
       </div>
     </div>
