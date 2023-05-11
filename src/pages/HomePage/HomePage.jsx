76fbda9667972461ae6ca347c04b9477bf4a816c
michudud <pzlc1236@gmail.com>
2023-05-11 19:31:33 +0200

    home page update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,20 @@
+import "./HomePage.style.css";
+import Header from "./../../components/Header";
+import Footer from "./../../components/Footer";
+import WelcomeSection from "./WelcomeSection";
+
 const HomePage = () => {
-  return <div className="HomePage"></div>;
+  return (
+    <div className="HomePage">
+      <Header />
+      <div className="PageContent">
+        <WelcomeSection />
+        <div>about</div>
+        <div>check menu</div>
+        <div>special offer</div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 export default HomePage;
