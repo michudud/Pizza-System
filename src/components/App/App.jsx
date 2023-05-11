a88a251cb48704e4afd654b22e7c0b7837408c8e
michudud <pzlc1236@gmail.com>
2023-05-11 21:37:49 +0200

    add menu page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import "./App.style.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
+import MenuPage from "../../pages/MenuPage/MenuPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/MenuPage/MenuPage.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import "./MenuPage.style.css";
+
+const MenuPage = () => {
+  return (
+    <div className="MenuPage">
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default MenuPage;
