04172c7f7df495af79fc62437997a007110ef99e
michudud <pzlc1236@gmail.com>
2023-05-11 16:51:45 +0200

    App update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,16 @@
-import "./App.css";
+import "./App.style.css";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/HomePage";
 
 const App = () => {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 const rootDiv = document.getElementById("root");
