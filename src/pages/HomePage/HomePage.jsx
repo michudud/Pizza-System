1ba54809b52ad280682461f83c92b030d8c48dd2
michudud <pzlc1236@gmail.com>
2023-05-13 12:11:29 +0200

    add news section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from "./../../components/Header";
 import Footer from "./../../components/Footer";
 import WelcomeSection from "./WelcomeSection";
 import MenuSection from "./MenuSection";
+import NewsSection from "./NewsSection";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <div className="PageContent">
         <WelcomeSection />
         <MenuSection />
+        <NewsSection />
         <div>about</div>
         <div>special offer</div>
       </div>
---
+++ src/pages/HomePage/NewsSection/NewsSection.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./NewsSection.style.css";
+
+const NewsSection = () => {
+  const [news, setNews] = useState([
+    "item1",
+    "item2",
+    "item3",
+    "item4",
+    "item5",
+  ]);
+  const newsNumber = 3;
+
+  const handleLeft = () => {
+    const changedNews = [...news];
+    changedNews.push(changedNews.shift());
+    setNews(changedNews);
+  };
+  const handleRight = () => {
+    const changedNews = [...news];
+    changedNews.unshift(changedNews.pop());
+    setNews(changedNews);
+  };
+
+  return (
+    <div className="NewsSection">
+      <div className="content">
+        <div className="description">
+          <h1>Latest News</h1>
+          <p>Check out what's going on in pizza world</p>
+        </div>
+        {/* <div className="categories"></div> */}
+        <div className="carousel">
+          <button className="carousel_button" onClick={handleLeft}>
+            {"<"}
+          </button>
+          {news.map((item, index) => {
+            if (index < newsNumber) {
+              return (
+                <div className="carousel_item" key={item}>
+                  {item}
+                </div>
+              );
+            }
+          })}
+
+          <button className="carousel_button" onClick={handleRight}>
+            {">"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NewsSection;
