70815c1defb21bf2b10920154e636eb70159f785
michudud <pzlc1236@gmail.com>
2023-05-16 15:55:59 +0200

    about section update

--- src/pages/HomePage/AboutSection/AboutSection.jsx
+++ src/pages/HomePage/AboutSection/AboutSection.jsx
@@ -1,4 +1,5 @@
 import "./AboutSection.style.css";
+import our_pizza from "./../.././../assets/images/our_pizza.jpg";
 import pizza_history_png from "./../../../assets/images/pizza_history_png.png";
 import pizza_history_svg from "./../../../assets/images/pizza_history_svg.svg";
 
@@ -7,10 +8,31 @@ const AboutSection = () => {
     <div className="AboutSection">
       <div className="content">
         <div className="description">
-          <h1>About Us</h1>
-          <p>about us section</p>
+          <h1>About Pizza</h1>
+          <p>You can always learn something new</p>
+        </div>
+        <div className="our_pizza">
+          <img className="our_pizza_img" src={our_pizza} />
+          <div className="our_pizza_description">
+            <h3>Our Pizza</h3>
+            <p>
+              Find out how we make our pizza step by step during a virtual
+              journey through our kitchen. Learn the ins and outs of selecting
+              ingredients, kneading dough and operating the oven. All to prepare
+              the best meal possible.
+            </p>
+          </div>
         </div>
         <div className="pizza_history">
+          <div className="pizza_history_description">
+            <h3>Pizza History</h3>
+            <p>
+              Who, how and where discovered the ideal way to consume a few
+              simple ingredients. This is not just a history lesson, it is an
+              expedition in search of the most important answers related to the
+              topic of pizza.
+            </p>
+          </div>
           <img
             className="pizza_history_img"
             src={pizza_history_svg}
@@ -18,11 +40,6 @@ const AboutSection = () => {
               this.src = pizza_history_png;
             }}
           />
-          <p className="pizza_history_description">pizza history</p>
-        </div>
-        <div className="our_pizza">
-          <img />
-          <p>our pizza</p>
         </div>
       </div>
     </div>
