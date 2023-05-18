947346b649cdcb47bd8a0076252fba7087a138c0
michudud <pzlc1236@gmail.com>
2023-05-18 22:11:22 +0200

    menu update

--- src/pages/MenuPage/menuItems.js
+++ src/pages/MenuPage/menuItems.js
@@ -1,62 +1,201 @@
+const assetsSrc = "./../../../assets/images/menu/";
+
 export const classicCategory = {
   name: "Classic",
   description: "classic category description",
   pizzas: [
     {
-      name: "Margharita",
-      ingredients: ["tomato sauce", "mozzarella"],
-      image: "img1",
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Sundried Tomatoes",
+        "Black Olives",
+        "Spinach",
+        "Parmesan",
+      ],
+      image: assetsSrc + "classic_1.jpg",
     },
     {
-      name: "Pepperoni",
-      ingredients: ["tomato sauce", "mozzarella", "pepperoni"],
-      image: "img2",
+      name: "na--me",
+      ingredients: [
+        "BBQ Sauce",
+        "Mozzarella",
+        "Seasoned Minced Beef",
+        "Jalapeños",
+      ],
+      image: assetsSrc + "classic_2.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Parma Ham",
+        "Tomatoes",
+        "Black Olives",
+        "Rucola",
+      ],
+      image: assetsSrc + "classic_3.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Creamy Sauce",
+        "Mozzarella",
+        "Jalapeños",
+        "Caramelized Onions",
+      ],
+      image: assetsSrc + "classic_4.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Ham",
+        "Mushrooms",
+        "Red Onions",
+        "Red Peppers",
+        "Black Olives",
+      ],
+      image: assetsSrc + "classic_5.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Tuna",
+        "Caramelized Onions",
+        "Black Olives",
+      ],
+      image: assetsSrc + "classic_6.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Ham",
+        "Mushrooms",
+        "Tomatoes",
+        "Yellow Peppers",
+        "Sweetcorn",
+        "Pickles",
+        "Black Olives",
+      ],
+      image: assetsSrc + "classic_7.jpg",
     },
   ],
 };
 
-export const vegetarianCetegory = {
+export const vegetarianCategory = {
   name: "Vegetarian",
-  description: "vegetarian category description",
+  description: "To meat, or not to meat, that is not the question here",
   pizzas: [
     {
-      name: "Veggie",
-      ingredients: ["tomato sauce", "mozzarella", "tomato"],
-      image: "img1",
+      name: "na--me",
+      ingredients: [
+        "BBQ Sauce",
+        "Mozzarella",
+        "Vegan Minced 'Meat'",
+        "Broccoli",
+        "Red Peppers",
+        "Red Onions",
+        "Smoked Cheese",
+      ],
+      image: assetsSrc + "vegetarian_1.jpg",
     },
     {
-      name: "Letucci",
-      ingredients: ["tomato sauce", "mozzarella", "letuce"],
-      image: "img2",
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Red Peppers",
+        "Yellow Peppers",
+        "Tomatoes",
+        "Sweetcorn",
+        "Jalapeños",
+        "Falafel",
+      ],
+      image: assetsSrc + "vegetarian_2.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Mozzarella",
+        "Mushrooms",
+        "Tomatoes",
+        "Green Peppers",
+        "Green Olives",
+        "Black Olives",
+      ],
+      image: assetsSrc + "vegetarian_3.jpg",
     },
   ],
 };
 
-export const veganCetegory = {
+export const veganCategory = {
   name: "Vegan",
-  description: "vegan category description",
+  description: "No animals were used to produce this pizzas",
   pizzas: [
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Vegan Mozzarella",
+        "Vegan Minced 'Meat'",
+        "Sweetcorn",
+        "Red Onions",
+        "Jalapeños",
+      ],
+      image: assetsSrc + "vegan_1.jpg",
+    },
     {
       name: "Veganium",
-      ingredients: ["tomato sauce", "mozzarella-like plant-based cheese"],
-      image: "img1",
+      ingredients: ["Tomato Sauce", "Vegan Mozzarella", "Fried Tofu", "Chives"],
+      image: assetsSrc + "vegan_veganium.jpg",
+    },
+    {
+      name: "na--me",
+      ingredients: [
+        "Tomato Sauce",
+        "Vegan Mozzarella",
+        "Vegan Ham",
+        "Red Peppers",
+        "Tomatoes",
+      ],
+      image: assetsSrc + "vegan_3.jpg",
     },
   ],
 };
 
-export const forbiddenCetegory = {
+export const forbiddenCategory = {
   name: "Forbidden",
-  description: "forbidden category description",
+  description: "After all... why not? Why shouldn't we make it",
   pizzas: [
     {
-      name: "Hawaii",
-      ingredients: ["tomato sauce", "mozzarella", "ananas"],
-      image: "img1",
+      name: "Momoa",
+      ingredients: ["Tomato Sauce", "Mozzarella", "Ham", "Pineapple"],
+      image: assetsSrc + "forbidden_momoa.jpg",
     },
     {
-      name: "Ibiza",
-      ingredients: ["tomato sauce", "mozzarella", "kiwi"],
-      image: "img2",
+      name: "Actinidiana",
+      ingredients: ["Tomato Sauce", "Mozzarella", "Ham", "Kiwi"],
+      image: assetsSrc + "forbidden_actinidiana.jpg",
+    },
+    {
+      name: "Easter Breakfast",
+      ingredients: [
+        "Mayonnaise  Sauce",
+        "Mozzarella",
+        "Ham",
+        "Boiled Eggs",
+        "Green Olives",
+      ],
+      image: assetsSrc + "forbidden_easterbreakfast.jpg",
     },
   ],
 };
