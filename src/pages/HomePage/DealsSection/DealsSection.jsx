62d82d88b9b0c29aee464eb43e893d0681ad954f
michudud <pzlc1236@gmail.com>
2023-05-24 13:34:05 +0200

    deals section update

--- src/pages/HomePage/DealsSection/DealsSection.jsx
+++ src/pages/HomePage/DealsSection/DealsSection.jsx
@@ -6,9 +6,21 @@ const DealsSection = () => {
       <div className="content">
         <div className="description">
           <h1>Amazing Deals</h1>
-          <p>check out our deals</p>
+          <p>It's just a wise decision</p>
         </div>
-        <div className="deals"></div>
+        <div className="deals">
+          <p>
+            We don't need your e-mail address and we are not sending any
+            newsletters.
+            <br /> Check out our regular and daily promotions or play our little
+            catching pizza game and get promo code for special offers.
+          </p>
+          <div className="promo_section">
+            <div className="regular_promo"></div>
+            <div className="daily_promo"></div>
+            <div className="pizza_game"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
