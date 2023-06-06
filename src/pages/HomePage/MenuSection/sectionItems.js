49115caddbbba9dec15ad98c1a4b60b80b992929
michudud <pzlc1236@gmail.com>
2023-06-06 21:04:45 +0200

    update menu section

---
+++ src/pages/HomePage/MenuSection/sectionItems.js
@@ -0,0 +1,28 @@
+const sectionsAssetsSrc = "./../../../assets/images/";
+
+export const allSections = [
+  {
+    name: "Classic",
+    description:
+      "Timeless classics straight from Italian kitchens. Simple combinations of delicious and fresh ingredients but also some more complex compositions that will satisfy almost every pizza lover.",
+    image: sectionsAssetsSrc + "category_classic.jpg",
+  },
+  {
+    name: "Vegetarian & Vegan",
+    description:
+      "Have you given up eating meat or animal products? For us it's not a problem, we will do everything to prepare a dish suitable for your preferences, while keeping the unique taste of delicious pizza.",
+    image: sectionsAssetsSrc + "category_vege.jpg",
+  },
+  {
+    name: "Special",
+    description:
+      "For the most discerning pizza absorbers, we have prepared several special positions. Each targets different tastes but we believe that if you are suitably abnormal you will be tempted by one of them.",
+    image: sectionsAssetsSrc + "category_special.jpg",
+  },
+  {
+    name: "Forbidden",
+    description:
+      "Some say that pineapple doesn't belong on pizza.... and we agree, but we can't prevent anyone from eating their favorite, most desired kind of pizza, so.... let it be.",
+    image: sectionsAssetsSrc + "category_forbidden.jpg",
+  },
+];
